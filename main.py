432ceb967085a52cb71c6b28335da3664b0723ba
James Crow <jamescrowvideo@gmail.com>
2017-04-01 01:46:12 -0500

    Added more basic structure

--- main.py
+++ main.py
@@ -15,13 +15,15 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
 
 header = """
     <!DOCTYPE html>
     <html>
     <head>
         <title>User Signup</title>
-        <link href="style.css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
 """
@@ -33,15 +35,33 @@ footer = """
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        loginForm = """
+            <div id="login" class="container">
+                <form action="/signup" method="post">
+                    <label id="username"><span>Username: <input type="text" name="userName"/></span></label>
+                    <label id="password"><span>Password: <input type="password" name="password"/></span></label>
+                    <label id="password-check"><span>Repeat Password: <input type="password" name="password-check"/></span></label>
+                    <label id="email"><span>Email: <input type="text" name="email"/></span></label>
+                    <input type="submit" class="submit" value="Create Your Account"/>
+                </form>
+            </div>
+        """
+        content = header + loginForm + footer
+        self.response.write(content)
+
+class Signup(webapp2.RequestHandler):
 
     def post(self):
-        isValid = {"userName":None, "password":None, "email":None}
+        isValid = {"userName":True, "password":True, "email":True} # CHANGE TO None AFTER TESTING
+
+        errors = ''
 
         userName = self.request.get("userName")
         password = self.request.get("password")
         password_check = self.request.get("password-check")
         email = self.request.get("email")
+
 
         if (isValid['userName'] == True and
             isValid['password'] == True and
@@ -50,8 +70,29 @@ class MainHandler(webapp2.RequestHandler):
             password == self.request.get("password") and
             email == self.request.get("email")):
 
+            #-- Escape userName
+            esc_userName = cgi.escape(userName)
+
+            success = """
+                <div class="container">
+                    <h1>Success!</h1>
+                    <h3>Welcome <strong>{0}</strong>!</h3>
+                    <p>You have successfully created and account.</p>
+                </div>
+            """.format(esc_userName)
+            content = header + success + footer
+            self.response.write(content)
+
+        elif errors != '':
+            self.redirect("/?" + errors)
+
+        else:
+            self.redirect("/?unknownError=True")
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/success')
+    ('/signup', Signup)
 ], debug=True)
